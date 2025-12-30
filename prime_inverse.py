f04563ee152dffa4471e6f8491390452d927b2bd
gino serpa <gino.serpa@gmail.com>
2025-12-30 11:12:35 -0600

    better code

--- prime_inverse.py
+++ prime_inverse.py
@@ -1,7 +1,20 @@
-def is_proper(num: int, den: int) -> bool:
-    if num >= den:
-        return False
-    return True
+def inverse_analyzer(n: int) -> tuple[list[int], list[int], int, int]:
+    q_s: list[int] = []
+    r_s: list[int] = []
+
+    r = 1
+    q, r = divmod(r * 10, n)
+    iter = 0
+    while r != 0 and r not in r_s:
+        q_s.append(q)
+        r_s.append(r)
+        q, r = divmod(r * 10, n)
+        iter += 1
+        if iter > n + 1:
+            print("iteration: ", iter)
+            break
+
+    return q_s, r_s, r, iter
 
 
-print(is_proper(200, 39))
+print(inverse_analyzer(213))
