896c8e14fc7a79430f4d3b267af9d395eff036b6
gino serpa <gino.serpa@gmail.com>
2025-12-31 10:54:15 -0600

    Simplified after debugging

--- prime_inverse.py
+++ prime_inverse.py
@@ -1,6 +1,3 @@
-import sys
-
-
 def inverse_analyzer(n: int) -> tuple[list[int], list[int], int, int]:
     q_s: list[int] = []
     r_s: list[int] = []
@@ -13,7 +10,7 @@ def inverse_analyzer(n: int) -> tuple[list[int], list[int], int, int]:
         q, r = divmod(r * 10, n)
         iter += 1
         if iter > 2 * n + 1:
-            print("iteration: ", iter)
+            print("iterations exceeded: ", iter, 2 * n + 1)
             break
     if r == 0:
         q_s.append(q)
@@ -26,11 +23,10 @@ def inverse_analyzer(n: int) -> tuple[list[int], list[int], int, int]:
     return q_s, r_s, r, iter
 
 
-if __name__ == "__main__":
-    n = int(sys.argv[1])
-    print(n)
-    q_s, r_s, r, iter = inverse_analyzer(n)
-    print("quotients: ", q_s)
-    print("residues: ", r_s)
-    print("last residue: ", r)
-    print("Number of iterations: ", iter)
+n = 7 * 40
+q_s, r_s, r, iter = inverse_analyzer(n)
+
+print("quotients: ", q_s)
+print("residues: ", r_s)
+print("last residue: ", r)
+print("Number of iterations: ", iter)
