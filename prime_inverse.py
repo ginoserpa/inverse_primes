7546a79dc053b12a758facc346c671e8811cf515
gino serpa <gino.serpa@gmail.com>
2026-01-04 10:29:17 -0600

    better output of subroutine

--- prime_inverse.py
+++ prime_inverse.py
@@ -1,3 +1,6 @@
+from unicodedata import decimal
+
+
 def inverse_analyzer(n: int) -> tuple[list[int], list[int], int, int]:
     q_s: list[int] = []
     r_s: list[int] = []
@@ -15,18 +18,23 @@ def inverse_analyzer(n: int) -> tuple[list[int], list[int], int, int]:
     if r == 0:
         q_s.append(q)
         r_s.append(r)
-        print("Finite decimal representation")
-        print("Number of non-zero decimal digits", len(q_s))
     elif r in r_s:
         q_s.append(q)
 
     return q_s, r_s, r, iter
 
 
-n = 7 * 40
-q_s, r_s, r, iter = inverse_analyzer(n)
-
-print("quotients: ", q_s)
-print("residues: ", r_s)
-print("last residue: ", r)
-print("Number of iterations: ", iter)
+for n in range(2, 14):
+    q_s, r_s, r, iter = inverse_analyzer(n)
+    print(n)
+    if r == 0:
+        print(f"finite decimal representation")
+        print(f"number of digits = {len(r_s)}")
+    print(f"quotients: {q_s}")
+    print(f"residues: {r_s}")
+    print("last residue: ", r)
+    print("Number of iterations: ", iter)
+    print(q_s)
+    decimal_representation = [str(i) for i in q_s]
+    print("repeated pattern", decimal_representation)
+    print(f"Compare to {1 / n}\n")
